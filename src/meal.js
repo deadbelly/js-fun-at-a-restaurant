435a2cc372e181166105770805b449fb2452bd67
Boone Epstein <booneepstein@gmail.com>
2020-10-08 16:10:37 -0500

    Update meal.js code to pass meal-test.js tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,42 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  return `Delicious ${item}`
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+}
+
+function addIngredients(newIngredient, ingredients) {
+  if (ingredients.indexOf(newIngredient) == -1){
+    ingredients.push(newIngredient)
+  }
+}
+
+function formatPrice(price){
+  return "$"+price.toString()
+}
+
+function decreasePrice(oldPrice) {
+  return oldPrice * .9
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
