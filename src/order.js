516807ccd463ee450d35171eb75db495b508b096
Boone Epstein <booneepstein@gmail.com>
2020-10-08 17:59:38 -0500

    Update order.js code to pass order-test.js tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,40 @@
+function takeOrder(order, listOfOrders) {
+  if (listOfOrders.length < 3){
+    listOfOrders.push(order)
+  }
+}
+
+function refundOrder(numberToRemove, listOfOrders) {
+  for (i = 0; i<listOfOrders.length; i++){
+    if (listOfOrders[i].orderNumber == numberToRemove){
+      listOfOrders.splice(i, 1)
+    }
+  }
+}
+
+function listItems(listOfOrders) {
+  var itemList = ""
+  for (i=0; i<listOfOrders.length; i++){
+    itemList += listOfOrders[i].item
+    if (listOfOrders[i+1]){
+      itemList += ", "
+    }
+  }
+  return itemList
+}
+
+function searchOrder(listOfOrders, item) {
+  for (i=0; i < listOfOrders.length; i++){
+    if (listOfOrders[i].item == item) {
+      return true
+    }
+  }
+  return false
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
