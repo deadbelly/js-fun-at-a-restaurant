e2671129bf3b78580e2020c76e719230671d0c6b
Boone Epstein <booneepstein@gmail.com>
2020-10-08 18:56:43 -0500

    Update restaurant.js code to pass restaurant-test.js tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,33 @@
+function createRestaurant(name) {
+  return {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    },
 
+  }
+}
+
+function addMenuItem(restaurant, item) {
+  if (restaurant.menus[item.type].indexOf(item) == -1){
+    restaurant.menus[item.type].push(item)
+  }
+}
+
+function removeMenuItem(restaurant, item, type) {
+  for (i=0; i < restaurant.menus[type].length; i++){
+    if (restaurant.menus[type][i].name == item){
+      restaurant.menus[type].splice(i, 1)
+      return `No one is eating our ${item} - it has been removed from the ${type} menu!`
+    }
+  }
+  return `Sorry, we don't sell ${item}, try adding a new recipe!`
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
