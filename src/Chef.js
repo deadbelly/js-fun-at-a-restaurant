6ffbd77f3b1ae1cf206fd8e56fd18c96db6ecdb7
Boone Epstein <booneepstein@gmail.com>
2020-10-08 19:39:50 -0500

    Update Chef.js code to pass Chef-test.js tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,21 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name
+    this.restaurant = restaurant
+  }
+  greetCustomer(name, isMorning){
+    if (isMorning) {
+      return `Good morning, ${name}!`
+    }else{
+      return `Hello, ${name}!`
+    }
+  }
+  checkForFood(foodItem) {
+    if (this.restaurant.menus[foodItem.type].indexOf(foodItem) > -1) {
+      return `Yes, we're serving ${foodItem.name} today!`
+    }else{
+      return `Sorry, we aren't serving ${foodItem.name} today.`
+    }
   }
 }
 
